b657982474ed6b3ba4708542bf21246151d46ece
thi1604 <thibeo1604@gmail.com>
2024-09-18 11:43:37 +0700

    Cai pug va ket noi database

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 import express, {Express, Request, Response} from "express";
+import { connectDatabase } from "./config/database";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app : Express = express();
 const port : number = 3000;
 
+
+connectDatabase();
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/topics", (req: Request, res: Response) => {
-  res.send("OK");
+  res.render("client/pages/topics/index.pug");
 });
 
 app.listen(port, ()=> {
